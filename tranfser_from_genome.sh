f730af5e999c174145bdc867c5ac6660eb0303d8
devd <devd@qgg.au.dk>
2026-07-15 12:01:00 +0200

    transfer

--- tranfser_from_genome.sh
+++ tranfser_from_genome.sh
@@ -3,8 +3,9 @@ REMOTE_BASE="/home/devd/worm-species/source"
 LOCAL_BASE="/home/devd/worm-species/"
 
 ssh "$REMOTE_HOST" "cd '$REMOTE_BASE' && find . -type f -print0" |
-parallel --ungroup -0 -j 8 --pipe --block 10M \
+parallel --ungroup -0 -j 4 --pipe --block 10M \
   rsync -avh --info=progress2 --ignore-existing \
+  --bwlimit=6500K \
   --from0 --files-from=- \
   "$REMOTE_HOST:$REMOTE_BASE/" \
   "$LOCAL_BASE/"
